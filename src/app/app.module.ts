a2c265a35821f2e6b2f6ff119dd7a63f0fcc224d
FlorianDitt <118379287+FlorianDitt@users.noreply.github.com>
2023-08-01 11:27:39 +0200

    feat: impressum Content&Style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {APP_ROUTES} from "./misc/app.routes";
 import {StoreModule} from "@ngrx/store";
 import {sygotchiReducer} from "./store/sygotchi.reducer";
+import { ImpressumComponent } from './impressum/impressum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthPageComponent,
-    CreationPageComponent
+    CreationPageComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule,
