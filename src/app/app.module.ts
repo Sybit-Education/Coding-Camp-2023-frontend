38532c652d1abdeab2f332619405869a97fa444e
Jan Steppacher <jan.steppacher@sybit.de>
2023-08-02 09:25:51 +0200

    fix: pr conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthInterceptor} from "./misc/auth.interceptor";
 import { AuthPageComponent } from './auth-page/auth-page.component';
-import { CreationPageComponent } from './creation-page/creation-page.component';
 import {RouterModule, RouterOutlet} from "@angular/router";
 import {ReactiveFormsModule} from "@angular/forms";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
@@ -23,13 +22,11 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     AuthPageComponent,
-    CreationPageComponent,
     LogoComponent,
-    CreationPageComponent,
     TeamSComponent,
     DsgvoPageComponent,
     ImpressumComponent,
-    SygotchiErstellenComponent
+    SygotchiErstellenComponent,
     ImpressumComponent,
     HeaderComponent
   ],
