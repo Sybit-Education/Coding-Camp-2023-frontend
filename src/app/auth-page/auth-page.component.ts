d92ad7d822712dfeb858052f6c6fa6c264f8bd32
Der-Zauberer <and.som282@gmail.com>
2023-08-02 15:44:00 +0200

    fix: login and register request error messages

--- src/app/auth-page/auth-page.component.ts
+++ src/app/auth-page/auth-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { Store, resultMemoize } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { WebsocketService } from '../services/websocket.service';
 import { HttpStatusCode } from '@angular/common/http';
 import { setSygotchi } from '../store/sygotchi.actions';
@@ -32,22 +32,22 @@ export class AuthPageComponent implements OnInit {
 
   register() {
     if(this.authForm.value.password === this.authForm.get('confirmPassword')?.value) {
-      this.authService.register(this.authForm.value.username, this.authForm.value.password)
-      .subscribe(
+      this.authService.register(this.authForm.value.username, this.authForm.value.password).subscribe(
         result => {
           localStorage.setItem('token', result["token"])
           localStorage.setItem('id', result["id"])
           this.router.navigate(['/create']);
         },
-      err => {
-        if (err.status === HttpStatusCode.Unauthorized) {
-          this.errorMessage = 'Benutzername wird bereits verwendet.'
-          this.showError = true;
-        } else {
-          this.errorMessage = 'Unerwarteter Fehler'
+        error => {
+          if (error.status === HttpStatusCode.Unauthorized) {
+            this.errorMessage = 'Benutzername wird bereits verwendet.'
+          } else if (error.error) {
+            this.errorMessage = 'Unerwarteter Fehler: ' + error.error.status + ' ' + error.error.title + ' ' + error.error.detail;
+          } else {
+            this.errorMessage = 'Unerwarteter Fehler'
+          }
           this.showError = true;
         }
-      }
       );
     } else {
     this.errorMessage = 'Passwörter stimmen nicht überein.'
@@ -72,14 +72,15 @@ export class AuthPageComponent implements OnInit {
             this.router.navigate(['/create'])
           })
       },
-      err => {
-        if (err.status === HttpStatusCode.BadRequest) {
+      error => {
+        if (error.status === HttpStatusCode.BadRequest) {
           this.errorMessage = 'Benutzername / Passwort ist falsch.'
-          this.showError = true
+        } else if (error.error) {
+          this.errorMessage = 'Unerwarteter Fehler: ' + error.error.status + ' ' + error.error.title + ' ' + error.error.detail;
         } else {
           this.errorMessage = 'Unerwarteter Fehler'
-          this.showError = true
         }
+        this.showError = true
       }
     )
   }
@@ -94,7 +95,5 @@ export class AuthPageComponent implements OnInit {
     this.showError = false
     this.authForm.reset()
   }
-
-
 
 }
