cd786fddbdb694e2ad4ded99e79fab330e7f3e8f
Der-Zauberer <and.som282@gmail.com>
2023-08-03 10:28:17 +0200

    fix: error messages

--- src/app/auth-page/auth-page.component.ts
+++ src/app/auth-page/auth-page.component.ts
@@ -53,7 +53,7 @@ export class AuthPageComponent implements OnInit {
         error => {
           if (error.status === HttpStatusCode.Unauthorized) {
             this.errorMessage = 'Benutzername wird bereits verwendet.'
-          } else if (rror.error && error.error.status && error.error.title && error.error.detail) {
+          } else if (error.error && error.error.status && error.error.title && error.error.detail) {
             this.errorMessage = 'Unerwarteter Fehler: ' + error.error.status + ' ' + error.error.title + ' ' + error.error.detail;
           } else {
             this.errorMessage = 'Unerwarteter Fehler'
