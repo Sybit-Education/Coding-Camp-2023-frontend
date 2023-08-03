a1aa631a20aa6cb8806832e0c7abdbd8346b6f6b
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2023-08-03 13:34:48 +0200

    blink animation added

--- src/app/show-sygotchi/show-sygotchi.component.ts
+++ src/app/show-sygotchi/show-sygotchi.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { ActionsService } from '../services/actions.service';
 import { SyGotchi } from '../entities/syGotchi';
 import * as paper from 'paper';
@@ -8,27 +8,38 @@ import { WebsocketService } from '../services/websocket.service';
 import { selectSygotchi } from '../store/sygotchi.selectors';
 import { setSygotchi } from '../store/sygotchi.actions';
 import {Store} from "@ngrx/store";
+import { interval } from 'rxjs';
+import { Point } from 'paper/dist/paper-core';
 
 @Component({
   selector: 'app-show-sygotchi',
   templateUrl: './show-sygotchi.component.html',
   styleUrls: ['./show-sygotchi.component.scss']
 })
-export class ShowSygotchiComponent implements OnInit {
+export class ShowSygotchiComponent implements AfterViewInit {
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  paperScope: paper.PaperScope;
   sygotchi: SyGotchi
-  canvas: any
   shapePath: any
   shape: any
 
+  pupilLeft: any
+  pupilRight: any
+  eyeLeft: any
+  eyeRight: any
+
+  animationFrame = 0
+
+  blinkScales = [ 1, 1, 1, 0.9, 0.5, 0.5, 1.5, 1.5, 1.5, 1.3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
   constructor(private actionsService: ActionsService, private store: Store, private wsService: WebsocketService) { }
 
-  ngOnInit(): void {
-    window['paper'] = paper;
+  ngAfterViewInit() {
+    this.paperScope = new paper.PaperScope();
+    this.paperScope.setup(this.canvas.nativeElement);
+
     new Project('canvas');
 
-    this.canvas = paper.project.activeLayer;
-
-    this.canvas = paper.project.activeLayer;
     this.store.select(selectSygotchi).subscribe(result => {
       this.sygotchi = result
 
@@ -43,9 +54,12 @@ export class ShowSygotchiComponent implements OnInit {
         this.buildSygotchi()
       }
     })
+
+    this.paperScope.view.onFrame = (event: any) => {
+      this.onAnimate();
+    };
+    this.paperScope.view.play();
   }
-
-
   buildSygotchi() {
     const shapeType = this.sygotchi.shape;
     const color = this.sygotchi.color;
@@ -74,8 +88,8 @@ export class ShowSygotchiComponent implements OnInit {
     }
     switch(shapeType){
       case 'TRIANGLE':
-        this.shape = new paper.Path.RegularPolygon({
-          center: paper.view.center,
+        this.shape = new this.paperScope.Path.RegularPolygon({
+          center: this.paperScope.view.center,
           sides: 3,
           radius: width / 2,
           fillColor: color
@@ -83,43 +97,77 @@ export class ShowSygotchiComponent implements OnInit {
         break;
 
       case 'RECTANGLE':
-        this.shape = new paper.Path.Rectangle({
-          point: paper.view.center.subtract(new paper.Point(width / 2, height / 2)),
+        this.shape = new this.paperScope.Path.Rectangle({
+          point: this.paperScope.view.center.subtract(new this.paperScope.Point(width / 2, height / 2)),
           size: [width, height],
           fillColor: color
         });
         break;
       case 'CIRCLE':
-        this.shape = new paper.Path.Circle({
-          center: paper.view.center,
+        this.shape = new this.paperScope.Path.Circle({
+          center: this.paperScope.view.center,
           radius: width / 2,
           fillColor: color
         });
         break;
     }
-    const eyeLeft = new paper.Path.Circle({
-      center: this.shape.position.subtract(new paper .Point(width / 4, height / 10)),
+    const eyeWhiteLeft = new this.paperScope.Path.Circle({
+      center: this.shape.position.subtract(new this.paperScope.Point(width / 4, height / 10)),
       radius: eyeSize,
       fillColor: 'white'
     })
-    const eyeRight = new paper.Path.Circle({
-      center: this.shape.position.subtract(new paper .Point(-width / 4, height / 10)),
+    const eyeWhiteRight = new this.paperScope.Path.Circle({
+      center: this.shape.position.subtract(new this.paperScope.Point(-width / 4, height / 10)),
       radius: eyeSize,
       fillColor: 'white'
     })
-    const pupilLeft = new paper.Path.Circle({
-      center: eyeLeft.position.add(new paper.Point(0, eyeSize * 0.1)),
+    this.pupilLeft = new this.paperScope.Path.Circle({
+      center: eyeWhiteLeft.position.add(new this.paperScope.Point(0, eyeSize * 0.1)),
       radius: pupilSize,
       fillColor: 'black'
     })
-    const pupilRight = new paper.Path.Circle({
-      center: eyeRight.position.add(new paper.Point(0, eyeSize * 0.1)),
+    this.pupilRight = new this.paperScope.Path.Circle({
+      center: eyeWhiteRight.position.add(new this.paperScope.Point(0, eyeSize * 0.1)),
       radius: pupilSize,
       fillColor: 'black'
     })
-    this.shapePath = new paper.Group({
-      children: [this.shape, eyeLeft, eyeRight, pupilRight, pupilLeft]
+
+    this.eyeRight = new this.paperScope.Group({
+      children: [this.shape, eyeWhiteRight, this.pupilRight]
     })
-    this.canvas.addChild(this.shapePath);
+
+    this.eyeLeft = new this.paperScope.Group({
+      children: [this.shape, eyeWhiteLeft, this.pupilLeft]
+    })
+
+    this.shapePath = new this.paperScope.Group({
+      children: [this.shape, this.eyeLeft, this.eyeRight]
+    })
+  }
+  onAnimate() {
+    if(this.eyeLeft && this.eyeRight) {
+      this.eyeLeft.scale(this.blinkScales[this.animationFrame]);
+      this.eyeRight.scale(this.blinkScales[this.animationFrame]);
+      this.animationFrame++;
+      if (this.animationFrame >= this.blinkScales.length) {
+        this.animationFrame = 0;
+      }
+    }
+
+  }
+
+  onMouseMove(e) {
+    //this.positionPupils(e);
+  }
+
+  positionPupils(event) {
+    //console.log(event)
+    var vector = new Point(event.x - 300, event.y - 300);
+    vector.length = Math.min(30, vector.length);
+    this.pupilLeft.position = this.eyeLeft.position + vector;
+
+    vector = new Point(event.x - 500, event.y - 300);
+    vector.length = Math.min(30, vector.length);
+    this.pupilRight.position = this.eyeRight.position + vector;
   }
 }
