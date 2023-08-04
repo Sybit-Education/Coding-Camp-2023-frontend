5a95defae0206c3e70e8e3b5edb6b63801aca580
Der-Zauberer <and.som282@gmail.com>
2023-08-04 12:31:27 +0200

    fix: reload

--- src/app/misc/auth.guard.ts
+++ src/app/misc/auth.guard.ts
@@ -8,7 +8,7 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    if (localStorage.getItem('token')) {
+    if (sessionStorage.getItem('token')) {
       return true
     } else {
       this.router.navigate(['/auth']);
